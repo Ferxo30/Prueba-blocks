b8c6b45d28f966c5eeb39b84f3e1286c9ea3773c
Ferxo30 <fboanergessotopaz@gmail.com>
2025-11-15 00:32:40 -0600

    Reportes a Excel

---
+++ base_accounting_kit_xlsx/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    "name": "Base Accounting Kit - Excel Reports",
+    "version": "18.0.1.0.0",
+    "summary": "Exportar a Excel los reportes de Base Accounting Kit",
+    "author": "Tú 😎",
+    "category": "Accounting",
+    "depends": [
+        "base_accounting_kit",
+    ],
+    "data": [
+        "views/account_report_view_form_xlsx.xml",
+    ],
+    "installable": True,
+    "application": False,
+    "license": "LGPL-3",
+}
